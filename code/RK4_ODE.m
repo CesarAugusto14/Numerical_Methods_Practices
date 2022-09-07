594441142f85e44b68d12359188f5f232388ee63
Cesar Sanchez <cesarasa@CesarsMacStudio.attlocal.net>
2022-09-07 09:46:40 -0500

    Further Changes

--- code/RK4_ODE.m
+++ code/RK4_ODE.m
@@ -14,12 +14,18 @@ function [t, x] = RK4_ODE(G, dt, t0, tf, x0)
     x(:,1) = x0;
     
     for i = 1:N-1
-        k1 = G(t(i), x(:, i));
-        k2 = G(t(i) + dt/2, x(:, i)+ k1*dt/2);
-        k3 = G(t(i) + dt/2, x(:, i)+ k2*dt/2);
-        k4 = G(t(i) + dt, x(:, i)+ k2*dt);
-        x(:, i+1) = x(:, i) + dt/6*(k1 + 2*k2 + 2*k3 + k4);
+        if x(3, i) <= 10
+            k1 = G(t(i), x(:, i));
+            k2 = G(t(i) + dt/2, x(:, i)+ k1*dt/2);
+            k3 = G(t(i) + dt/2, x(:, i)+ k2*dt/2);
+            k4 = G(t(i) + dt, x(:, i)+ k2*dt);
+            x(:, i+1) = x(:, i) + dt/6*(k1 + 2*k2 + 2*k3 + k4);
+        else
+            break
+        end
+        
     end
     x = x';
+    
 % Code developed by Cesar Sanchez for Fall 2022. 
 end
--- code/tarea.m
+++ code/tarea.m
@@ -1,18 +1,18 @@
 clc; clear variables; close all
 
-C = 5; Vo = 10e3; L = 30e-3; Rl = 50e-3; Ll=700e-9;
+C = 5; Vo = 100e3; L = 1e-6; Rl = 50e-3; Ll=0.7e-6;
 m = 10;
 x0 = [0;Vo/L;0;0];
-dt = 1e-2;
-tf = 1;
-[t1, x1] = ode23(@(t,x) railgun(t,x, C, L, Ll, Rl, m), 0:dt:tf, x0);
+dt = 1e-7;
+tf = 10e-3;
+%[t1, x1] = ode23(@(t,x) railgun(t,x, C, L, Ll, Rl, m), 0:dt:tf, x0);
 [t2, x2] = RK4_ODE(@(t,x) railgun(t,x, C, L, Ll, Rl, m), dt, 0, tf, x0);
 
 % Extracting the resulting vectors for Ode45:
-I1 = x1(:,1);
-dI1 = x1(:,2); 
-x_pos1 = x1(:,3);
-u1 = x1(:,4);
+% I1 = x1(:,1);
+% dI1 = x1(:,2); 
+% x_pos1 = x1(:,3);
+% u1 = x1(:,4);
 
 % Extracting the resulting vectors for RK4:
 I2 = x2(:,1);
@@ -23,14 +23,17 @@ u2 = x2(:,4);
 % Plotting:
 figure()
 hold on
-plot(t1, I1/1000, 'r-x', 'linewidth', 2, 'MarkerSize', 8)
+% plot(t1, I1/1000, 'r-x', 'linewidth', 2, 'MarkerSize', 8)
 plot(t2, I2/1000, 'b-.', 'linewidth', 2)
-legend('Solution from ode23', 'Solution from RK4')
+legend('Solution from RK4')
 xlabel('Time [s]','Interpreter','latex')
 ylabel('Current on the Circuit [kA]','Interpreter','latex')
 set(gca, 'FontName', 'Times', 'FontSize', 15)
 title('Current on the Railgun Circuit')
-grid on
+grid on 
 
-figure()
+figure(2)
+plot(t2, u2)
+
+figure(3)
 plot(t2, x_pos2)
