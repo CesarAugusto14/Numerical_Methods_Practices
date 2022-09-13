f13c5743dcd7ca8caa896672ff5bf6cd0fe5b820
Cesar Sanchez <cesarasa@CesarsMacStudio.attlocal.net>
2022-09-13 17:51:02 -0500

    Things

--- code/testing_higher_order.m
+++ code/testing_higher_order.m
@@ -1,7 +1,7 @@
 clc; clear variables; close all;
 
 % Specifications of the desired Circuit.
-R = 20; C = 14e-6; Vs = 5; 
+R = 2e3; C = 14e-6; Vs = 5; 
 
 % Initial voltage.
 Vo = 0;
@@ -10,10 +10,22 @@ Vo = 0;
 % simulation. 
 tau = R*C; 
 tf = 5*tau;
+F = @(t, v) 1/R/C*(Vs - v);
 
-F = @(t, v) 1/R/C*(Vs*sin(377*t) - v);
+[t, v] = dp_rk45a_ODE(F, 0, tf, Vo, 1e-8, 4e-4);
+[t2, v2] = RK4_ODE(F, 1e-5, 0, tf, Vo);
 
+% Exact solution (known by our knowledge on Electrical Circuits):
+tplot = 0:1e-5:tf; vplot = Vs + (Vo - Vs)*exp(-tplot/R/C);
 
-[t, x] = dp_rk45a_ODE(F, 0, tf, Vo, 1);
-
-plot(t, x)
+figure(1)
+hold on
+plot(t*1000, v, 'r-', 'linewidth', 2, 'MarkerSize', 10)
+plot(t2*1000, v2,'g-.','LineWidth',2)
+plot(tplot*1000, vplot, 'k-', 'linewidth',2)
+legend('RK45a', 'RK4','Exact Solution', 'Location','southeast')
+title('Comparison Between Numerical and Exact with RK methods')
+ylabel('$V_c$ [V]', 'Interpreter','latex')
+xlabel('$t$ [ms]', 'Interpreter','latex')
+set(gca, 'FontName', 'Times', 'FontSize', 15)
+grid on
